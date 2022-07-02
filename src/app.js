92cc3ddb645a357b9f6fc79ce3a599e5d4e2d4e9
KarolJaskolka <jaskolka.karol@gmail.com>
2022-07-03 00:23:46 +0200

    Follow security practices

--- src/app.js
+++ src/app.js
@@ -21,9 +21,27 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
+const helmet = require("helmet");
+
+app.use(helmet());
+app.disable("x-powered-by");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const authRoutes = require("./routes/auth");
 
 app.use("/api/auth", authRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    error_message: "error.not-found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error_message: "error.internal-server-error",
+    error_log: err.message,
+  });
+});
