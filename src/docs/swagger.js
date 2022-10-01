e5e1a9ed7878bc4e867ba0d501b3654f662ffc67
KarolJaskolka <jaskolka.karol@gmail.com>
2022-10-01 11:40:12 +0200

    Update package.json and api version

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -3,7 +3,7 @@ module.exports = {
     openapi: "3.0.0",
     info: {
       title: "Triple Quiz API",
-      version: "0.0.0",
+      version: "1.1.1",
     },
   },
   apis: ["./src/docs/**/*.yaml"],
